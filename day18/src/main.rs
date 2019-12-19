5eeb998344da68240b5402e4d1f0d150cc203d93
DomiDre <homiedomi@gmail.com>
2019-12-19 11:57:57 +0100

    Completed day18

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -28,32 +28,10 @@ fn part1() {
 
 fn part2() {
     let maze = Maze::new(read_input("./updated_input").unwrap());
+    maze.collect_all_keys();
 }
 
 fn main() {
     part1();
-    // part2();
-    // // determine all paths to finish the maze
-    // // init with no keys collected
-    // let collected_keys = Vec::new();
-    // // and all doors closed
-    // let closed_doors = maze.doors.clone();
-    // let paths = maze.get_paths_length_dfs(maze.start_idx, collected_keys, closed_doors, 0);
-    // println!("There are {} possible paths", paths.len());
-    // let mut shortest_path = &paths[0].0;
-    // let mut shortest_length = paths[0].1;
-    // for path in paths.iter() {
-    //     if path.1 < shortest_length {
-    //         shortest_path = &path.0;
-    //         shortest_length = path.1;
-    //     }
-    // }
-    // println!("The shortest path is {:?} with length {}", shortest_path, shortest_length);
-
-    // maze.get_paths_length_dijkstra();
-
-    // println!("{:?}", maze.paths_collect_all_keys());
-    // let path = maze.find_shortest_connection(
-    //     maze.start_idx, *maze.keys.get(&'a').unwrap()
-    // );
+    part2();
 }
--- day18/src/maze.rs
+++ day18/src/maze.rs
@@ -9,7 +9,7 @@ pub type Map = [[char; MAP_DIM_Y]; MAP_DIM_X];
 /// For final output also keep track of the order in that the keys are collected
 #[derive(Clone, Debug)]
 struct State {
-    pub position: usize,
+    pub position: Vec<usize>,
     pub distance: usize,
     pub collected_keys: HashSet<char>,
     pub keys_in_order: Vec<char>,
@@ -17,7 +17,7 @@ struct State {
 
 impl State {
     pub fn new(
-        position: usize,
+        position: Vec<usize>,
         distance: usize,
         collected_keys: HashSet<char>,
         keys_in_order: Vec<char>,
@@ -59,7 +59,7 @@ pub struct Maze {
     pub grid: Map,
     pub adjacency_list: HashMap<usize, Vec<usize>>,
     pub point_of_interests: HashMap<usize, char>,
-    pub start_idx: usize,
+    pub start_idx: Vec<usize>,
     pub keys: HashMap<char, usize>,
     pub doors: HashMap<char, usize>,
 }
@@ -72,7 +72,7 @@ impl Maze {
         let mut keys = HashMap::new();
         let mut doors = HashMap::new();
         let mut point_of_interests = HashMap::new();
-        let mut start_idx = 0;
+        let mut start_idx = Vec::new();
         for j in 0..MAP_DIM_Y {
             for i in 0..MAP_DIM_X {
                 let c = grid[i][j];
@@ -87,12 +87,18 @@ impl Maze {
                     _ => {
                         if c.is_lowercase() {
                             keys.insert(c, idx);
+                            point_of_interests.insert(idx, c);
                         } else if c.is_uppercase() {
                             doors.insert(c, idx);
+                            point_of_interests.insert(idx, c);
                         } else if c == '@' {
-                            start_idx = idx;
+                            if start_idx.len() > 9 {
+                                panic!("Maze only supports 10 start positions (for now).");
+                            }
+                            // replace @ symbol by chars '0'..'9'
+                            point_of_interests.insert(idx, digit_to_char(start_idx.len()));
+                            start_idx.push(idx);
                         }
-                        point_of_interests.insert(idx, c);
                     }
                 };
 
@@ -112,7 +118,7 @@ impl Maze {
             }
         }
 
-        assert_ne!(start_idx, 0, "Start position not defined in maze");
+        assert_ne!(start_idx.len(), 0, "No start position defined in maze");
 
         Maze {
             grid,
@@ -186,6 +192,44 @@ impl Maze {
         Some(path)
     }
 
+    // /// get all positions that are reachable and how many steps to get there
+    // fn get_reachable_keys(&self, start_idx: usize, collected_keys: &HashSet<char>) -> Vec<(char, usize)> {
+    //     let mut reachable_keys = Vec::new();
+    //     'for_keys: for (i, (key, key_pos)) in self.keys.iter().enumerate() {
+    //         let path_opt = self
+    //         .find_shortest_connection(self.start_idx[0], *key_pos);
+    //         // key is unreachable if path is none
+    //         if path_opt.is_none() {
+    //             continue
+    //         }
+    //         let path = path_opt.unwrap();
+    //         let steps = path.len() - 1;
+
+    //         // check for doors on path
+    //         for i in 1..path.len() {
+    //             // is path a point of interest?
+    //             let opt_poi = self.point_of_interests.get(&path[i]);
+    //             if opt_poi.is_some() {
+    //                 let poi = opt_poi.unwrap();
+    //                 // is poi a door?
+    //                 if poi.is_ascii_uppercase() {
+    //                     // if key for door is not collected -> this key is not reachable
+    //                     if collected_keys.get(&poi.to_ascii_lowercase()).is_none() {
+    //                         continue 'for_keys;
+    //                     }
+    //                 }
+    //                 // possibly add here if anything should be done if another poi is on path
+    //             }
+    //         }
+    //         reachable_keys.push((*key, steps));
+    //     }
+    //     // return trsvhsblr keys in order of shortest distance
+    //     reachable_keys.sort_by_key(|k| k.1);
+    //     reachable_keys
+    // }
+
+    /// for all keys determine distance to every other key (that has a reachable path)
+    /// and which doors are along the path
     pub fn get_distances_and_blocking_doors(
         &self,
     ) -> (
@@ -194,48 +238,23 @@ impl Maze {
     ) {
         let mut distances: HashMap<char, Vec<(char, usize)>> = HashMap::new();
         let mut blocking_doors: HashMap<(char, char), Vec<char>> = HashMap::new();
-
-        for (i, (key, key_pos)) in self.keys.iter().enumerate() {
-            // check distance from start
-            let path = self
-                .find_shortest_connection(self.start_idx, *key_pos)
-                .unwrap();
-            let steps = path.len() - 1;
-            distances
-                .entry('@')
-                .or_insert(Vec::new())
-                .push((*key, steps));
-            // check if on path, blocking doors are present
-            let mut door_on_path = Vec::new();
-            for i in 1..path.len() {
-                // is path a point of interest?
-                let opt_poi = self.point_of_interests.get(&path[i]);
-                if opt_poi.is_some() {
-                    let poi = opt_poi.unwrap();
-                    if self.doors.get(poi).is_some() {
-                        door_on_path.push(*poi);
-                    }
+        
+        // check every robot
+        for idx_robot in 0..self.start_idx.len() {
+            for (i, (key, key_pos)) in self.keys.iter().enumerate() {
+                // check distance from start
+                let path_opt = self
+                    .find_shortest_connection(self.start_idx[idx_robot], *key_pos);
+                // ignore generally unreachable keys
+                if path_opt.is_none() {
+                    continue
                 }
-            }
-            if door_on_path.len() > 0 {
-                blocking_doors.insert(('@', *key), door_on_path);
-            }
-
-            // check distance between keys
-            for (other_key, other_key_pos) in self.keys.iter().skip(i + 1) {
-                let path = self
-                    .find_shortest_connection(*key_pos, *other_key_pos)
-                    .unwrap();
+                let path = path_opt.unwrap();
                 let steps = path.len() - 1;
                 distances
-                    .entry(*key)
-                    .or_insert(Vec::new())
-                    .push((*other_key, steps));
-                distances
-                    .entry(*other_key)
+                    .entry(digit_to_char(idx_robot))
                     .or_insert(Vec::new())
                     .push((*key, steps));
-
                 // check if on path, blocking doors are present
                 let mut door_on_path = Vec::new();
                 for i in 1..path.len() {
@@ -249,8 +268,43 @@ impl Maze {
                     }
                 }
                 if door_on_path.len() > 0 {
-                    blocking_doors.insert((*key, *other_key), door_on_path.clone());
-                    blocking_doors.insert((*other_key, *key), door_on_path);
+                    blocking_doors.insert((digit_to_char(idx_robot), *key), door_on_path);
+                }
+    
+                // check distance between keys
+                for (other_key, other_key_pos) in self.keys.iter().skip(i + 1) {
+                    let path_opt = self
+                        .find_shortest_connection(*key_pos, *other_key_pos);
+                    if path_opt.is_none() {
+                        continue
+                    }
+                    let path = path_opt.unwrap();
+                    let steps = path.len() - 1;
+                    distances
+                        .entry(*key)
+                        .or_insert(Vec::new())
+                        .push((*other_key, steps));
+                    distances
+                        .entry(*other_key)
+                        .or_insert(Vec::new())
+                        .push((*key, steps));
+    
+                    // check if on path, blocking doors are present
+                    let mut door_on_path = Vec::new();
+                    for i in 1..path.len() {
+                        // is path a point of interest?
+                        let opt_poi = self.point_of_interests.get(&path[i]);
+                        if opt_poi.is_some() {
+                            let poi = opt_poi.unwrap();
+                            if self.doors.get(poi).is_some() {
+                                door_on_path.push(*poi);
+                            }
+                        }
+                    }
+                    if door_on_path.len() > 0 {
+                        blocking_doors.insert((*key, *other_key), door_on_path.clone());
+                        blocking_doors.insert((*other_key, *key), door_on_path);
+                    }
                 }
             }
         }
@@ -262,14 +316,18 @@ impl Maze {
 
     pub fn collect_all_keys(&self) {
         let (distances, blocking_doors) = self.get_distances_and_blocking_doors();
-
-        let mut queue: Vec<State> = Vec::new();
-        let initial_state = State::new(self.start_idx, 0, HashSet::new(), Vec::new());
-        let mut known_states: HashSet<State> = HashSet::new();
+        // for (start_char, distances) in distances.iter() {
+        //     println!("{} -> {:?}", start_char, distances);
+        // }
+        let num_all_keys = self.keys.len(); // number of keys that need to be collected
+        let num_robots = self.start_idx.len(); // number of robots that can move
+        
+        let mut queue: Vec<State> = Vec::new(); // sorted queue of states 
+        let initial_state = State::new(self.start_idx.clone(), 0, HashSet::new(), Vec::new());
         queue.push(initial_state);
 
-        let num_all_keys = self.keys.len();
-        let mut counter = 0;
+        let mut known_states: HashSet<State> = HashSet::new(); // keep track of visited states
+        let mut counter = 0; // state counter for debugging
         loop {
             let state = queue.remove(0);
 
@@ -283,55 +341,61 @@ impl Maze {
                 break;
             }
 
-            let state_key = self.point_of_interests.get(&state.position).unwrap();
+            // test to move every robot
+            for robot_idx in 0..num_robots {
+                // println!("{}", state.position[robot_idx]);
+                // test every possible move to other keys from current position
+                let state_key_opt = self.point_of_interests.get(&state.position[robot_idx]);
 
-            'node_neighbour_loop: for (other_key, distance) in
-                distances.get(state_key).unwrap().iter()
-            {
-                // check if key has already been collected
-                if state.collected_keys.get(other_key).is_some() {
-                    continue;
-                }
-                // first check if this key is even reachable
-                let blockage_opt = blocking_doors.get(&(*state_key, *other_key));
-                if blockage_opt.is_some() {
-                    let blocked = blockage_opt.unwrap();
-                    for blocked_door in blocked.iter() {
-                        // check if key for blocked_door was collected
-                        if state
-                            .collected_keys
-                            .get(&blocked_door.to_ascii_lowercase())
-                            .is_none()
-                        {
-                            continue 'node_neighbour_loop;
+                let state_key = state_key_opt.unwrap();
+                //.unwrap();
+                'node_neighbour_loop: for (other_key, distance) in
+                    distances.get(state_key).unwrap().iter()
+                {
+                    // check if key has already been collected
+                    if state.collected_keys.get(other_key).is_some() {
+                        continue;
+                    }
+                    // first check if this key is even reachable
+                    let blockage_opt = blocking_doors.get(&(*state_key, *other_key));
+                    if blockage_opt.is_some() {
+                        let blocked = blockage_opt.unwrap();
+                        for blocked_door in blocked.iter() {
+                            // check if key for blocked_door was collected
+                            if state
+                                .collected_keys
+                                .get(&blocked_door.to_ascii_lowercase())
+                                .is_none()
+                            {
+                                continue 'node_neighbour_loop;
+                            }
                         }
                     }
+                    // it's valid to collect the key, add new state
+                    let mut new_state = state.clone();
+                    new_state.position[robot_idx] = *self.keys.get(other_key).unwrap();
+                    new_state.distance += distance;
+                    new_state.collected_keys.insert(*other_key);
+                    new_state.keys_in_order.push(*other_key);
+                    queue.push(new_state)
                 }
-                let mut collected_keys = state.collected_keys.clone();
-                collected_keys.insert(*other_key);
-                let mut keys_in_order = state.keys_in_order.clone();
-                keys_in_order.push(*other_key);
-                let new_state = State::new(
-                    *self.keys.get(other_key).unwrap(),
-                    state.distance + distance,
-                    collected_keys,
-                    keys_in_order,
-                );
-
-                queue.push(new_state)
             }
             queue.sort_by_key(|k| k.distance);
+            // bottle neck is sort if queue becomes too long
+            // if queue is > 50_000, drop the last states
+            // ... this is not generally correct and may result in wrong results
+            // gotta optimize the code otherwise...
+            if counter % 10000 == 0  && queue.len() > 50000 {
+                queue.drain(50000..);
+            }
             counter += 1;
-            if counter % 1000 == 0 {
-                // println!("---");
+            if counter % 10000 == 0 {
                 println!(
-                    "Iteration: {}, Number of candidate states: {}",
+                    "Iteration: {}, Shortest Distance Travelled: {}, Number of candidate states: {} ... Still Calculating...",
                     counter,
+                    state.distance,
                     queue.len()
                 );
-                // for path in queue.iter().take(10) {
-                //     println!("{:?}", path);
-                // }
             }
             known_states.insert(state);
         }
@@ -340,4 +404,8 @@ impl Maze {
 
 fn grid_to_idx(x: usize, y: usize) -> usize {
     x + y * MAP_DIM_X
+}
+
+fn digit_to_char(digit: usize) -> char {
+    ((digit as u8) + b'0') as char
 }
